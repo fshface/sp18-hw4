b67376fd01e156e6a3c1e2ee65bd2e505eb75cc1
Brandon <brandyfongfong@berkeley.edu>
2018-03-06 19:53:34 -0800

    still need help

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,15 @@ class TodosController < ApplicationController
 	def new
     	@todo = Todo.new
   	end
+
+  	def create
+    	@todo = Todo.new(todo_params)
+    	redirect_to pages_home_path
+  	end
+
+	private
+
+	def todo_params
+		params.require(:todo).permit(:tasks, :finished)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'todos/new'
-  post 'todos', to: 'todos#new'
+  post 'todos/new', to: 'todos#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
